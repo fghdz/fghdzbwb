6c9c378a54cae1f191beb034fb74f02ceee51aa9
fghdz <63494815+fghdz@users.noreply.github.com>
2023-02-04 13:49:51 +0800

    Add files via upload

---
+++ js/WebsiteAnnouncement.js
@@ -0,0 +1,66 @@
+
+function myfunction(){
+	  sj = Math.floor(Math.random()*5);
+	  console.log(sj);
+	   if(sj==0){
+     
+  new Vue({
+		el:"#Websiteannouncement",
+		data:{
+			不会真的有人加载不出来吧:"这网站没什么人来吧，那正好",	
+		}
+		})
+
+	  }
+
+	  else if(sj==1){
+ new Vue({
+		el:"#Websiteannouncement",
+		data:{
+			不会真的有人加载不出来吧:"唉，话说真的有人会看见这行字吗",	
+		}
+		})
+	  }
+
+
+	    else if(sj==2){
+     new Vue({
+		el:"#Websiteannouncement",
+		data:{
+			不会真的有人加载不出来吧:"哦？你问公告的变化的概率？你试试就知道咯",	
+		}
+		})
+	  } 
+	  
+	  
+	  else if(sj==3){
+     new Vue({
+		el:"#Websiteannouncement",
+		data:{
+			不会真的有人加载不出来吧:"求三连唉，姆~点个赞也行，实在不行就发个弹幕或评论吧",	
+		}
+		})
+	  }
+
+	  else if(sj==4){
+     new Vue({
+		el:"#Websiteannouncement",
+		data:{
+			不会真的有人加载不出来吧:"为什么加载慢？养不起服务器咯",	
+		}
+		})
+	  }
+
+
+
+	  else{
+	  	 new Vue({
+		el:"#Websiteannouncement",
+		data:{
+			不会真的有人加载不出来吧:"据说公告一直在变化咯",		
+		}
+		}) 
+	  }
+}
+window.onload = myfunction;
+
